8f59cc03a076aa84026499d1f75d950dc53b160b
Elżbieta Stolarczyk <elastolarczyk_1992@vp.pl>
2020-04-05 17:59:01 +0200

    my first repository

---
+++ Fibonacci.java
@@ -0,0 +1,42 @@
+package main;
+
+public class Fibonacci {
+
+    private  int valN; // pole do przechowywania zmiennej n
+
+    public Fibonacci(int valN)
+    {
+        this.valN = valN;
+    }
+
+    public int fibb(int n) {
+
+        if (n == 0 || n == 1)
+        {
+            return n;
+        }
+
+        else if(n < 0)
+        {
+            throw new IllegalArgumentException("Liczba nie może być mniejsza od zera");
+        }
+        else
+        {
+            return fibb(n - 1) + fibb(n - 2);//dlaczego mając tu wartość valN wychodzą błędne wyniki
+        }
+    }
+/*
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj liczbe: ");
+        int n = scan.nextInt();
+        System.out.println("Podałeś liczbe: " + n);
+
+        for(int i = 0; i < n; i++){
+            System.out.print(fibb(i) + " ");
+        }
+
+ */
+}
+
+
