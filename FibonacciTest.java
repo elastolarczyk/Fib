f023f6e67aa559673d4dfe30cdfce99eac87d637
elastolarczyk <50515171+elastolarczyk@users.noreply.github.com>
2020-04-05 18:37:53 +0200

    Add files via upload

---
+++ FibonacciTest.java
@@ -0,0 +1,51 @@
+package main;
+
+import org.junit.Test;
+
+// dodaj pole do klasy Fibonacci, w którym bedziesz przechowywała przekazana wartosc n
+import static org.junit.Assert.assertEquals;
+
+public class FibonacciTest {
+
+    int n;
+
+    Fibonacci test1 = new Fibonacci(n);
+
+    @Test
+    public void fibbTest1()
+    {
+        assertEquals(0, test1.fibb(0));
+    }
+
+    @Test
+    public void fibbTest2()
+    {
+        assertEquals(1, test1.fibb(1));
+    }
+
+    @Test
+    public void fibbTest3()
+    {
+        assertEquals(1, test1.fibb(2));
+    }
+
+    @Test
+    public void fibbTest4()
+    {
+        assertEquals(8, test1.fibb(6));
+    }
+
+    @Test
+    public void fibbTest5()
+    {
+        assertEquals(4181, test1.fibb(19));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void fibbShouldGiveAnExceptionTest6()
+    {
+        test1.fibb(-3);
+    }
+}
+
+
